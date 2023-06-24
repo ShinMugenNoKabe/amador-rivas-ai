7f5e1ba361c47af37070b10b96344e8017a1dbcf
ShinMugenNoKabe <rufisercay@gmail.com>
2023-06-24 21:24:40 +0200

    Removed first call to prevent Too many requests

--- app/main.py
+++ app/main.py
@@ -2,13 +2,10 @@ from schedule_twit_every_hour import schedule_twit_every_hour
 import schedule
 import time
 import logging
-import threading
 
 
 def main():
     logging.info(f"Bot is on")
-    
-    schedule_twit_every_hour()
     
     schedule.every(3).hours.at(":00").do(schedule_twit_every_hour)
     
