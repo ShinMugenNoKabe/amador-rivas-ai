8fcad40fc77d8685789def8b07b9159b38dc95e4
ShinMugenNoKabe <rufisercay@gmail.com>
2023-06-24 18:06:41 +0200

    Implemented threading and also twit every hour

--- app/main.py
+++ app/main.py
@@ -2,12 +2,18 @@ from schedule_twit_every_hour import schedule_twit_every_hour
 import schedule
 import time
 import logging
+import threading
+
+
+def start_thread(func):
+    job = threading.Thread(target=func)
+    job.start()
 
 
 def main():
     logging.info(f"Bot is on")
     
-    schedule.every(20).seconds.do(schedule_twit_every_hour)
+    schedule.every().minute.at(":30").do(start_thread, schedule_twit_every_hour)
     
     while True:
         schedule.run_pending()
